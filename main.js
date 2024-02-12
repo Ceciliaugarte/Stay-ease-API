3a594fea0362a70295004fcb07225c3de3f170cd
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-02-12 19:03:32 -0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const http = require("http");
 const APP_PORT = process.env.APP_PORT || 3000;
 const express = require("express");
 
 const app = express();
+
+app.listen(APP_PORT, () => {
+  console.log(
+    `\nServer is running on port ${APP_PORT}, on http://www.localhost:${APP_PORT} \n`
+  );
+});
