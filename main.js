a3c63c2b688498fb8c5201a503910a8a83d820b7
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-02-13 11:29:50 -0300

    routes

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const APP_PORT = process.env.APP_PORT || 3000;
 const express = require("express");
+const cors = require("cors");
+/* const routes = require("./"); */
 
 const app = express();
+app.use(cors());
 
 app.listen(APP_PORT, () => {
   console.log(
