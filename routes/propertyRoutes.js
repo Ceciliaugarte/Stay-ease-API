8016c05f587977d0d9472dd5a13385b7c948ef68
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-02-26 17:39:21 -0300

    properties auth

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const propertyController = require("../controllers/propertyController");
+const { expressjwt: checkJwt } = require("express-jwt");
+const checkToken = checkJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: [process.env.JWT_ALGORITHMS],
+});
 
 router.get("/", propertyController.index);
 router.get("/:id", propertyController.show);
+
+router.use(checkToken);
+
 router.post("/", propertyController.store);
 router.patch("/:id", propertyController.update);
 router.delete("/:id", propertyController.destroy);
