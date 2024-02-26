7e7719ad20654376c7b4e042b4daba398aa0d0b0
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-02-26 17:22:02 -0300

    getToken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,14 @@
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 async function getToken(req, res) {
-  return res.end("WELCOME TO HOME");
+  const user = await User.findOne({ where: { email: req.body.email } });
+  if (!user) return res.json("Wrong credentials!");
+  const verifyPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!verifyPassword) res.json("Wrong credentials!");
+  const token = jwt.sign({ sub: user.id }, process.env.JWT_SECRET);
+  res.json({ token });
 }
 
 module.exports = {
