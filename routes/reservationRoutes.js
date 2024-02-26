17b9e5adb8af43baa3f947a99c68dd18c9f109b5
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-02-26 17:42:27 -0300

    user and reservation auth

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const reservationController = require("../controllers/reservationController");
+const { expressjwt: checkJwt } = require("express-jwt");
+const checkToken = checkJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: [process.env.JWT_ALGORITHMS],
+});
+
+router.use(checkToken);
 
 router.get("/", reservationController.index);
 router.get("/:id", reservationController.show);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { expressjwt: checkJwt } = require("express-jwt");
+const checkToken = checkJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: [process.env.JWT_ALGORITHMS],
+});
 
+router.post("/", userController.store);
+
+router.use(checkToken);
 router.get("/", userController.index);
 router.get("/:id", userController.show);
-router.post("/", userController.store);
 router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
 
