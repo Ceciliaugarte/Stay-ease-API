081533ffe96b9fffd3872841c7712383276f92eb
Ceciliaugarte <cecilia.ugartemendia@student.ha.dev>
2024-02-26 17:34:34 -0300

    auth for comments

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.get("/", authController.getToken);
+router.post("/", authController.getToken);
 
 module.exports = router;
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
+const { expressjwt: checkJwt } = require("express-jwt");
+const checkToken = checkJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: [process.env.JWT_ALGORITHMS],
+});
 
-router.get("/", commentController.index);
-router.get("/:id", commentController.show);
+router.use(checkToken);
+
+/* router.get("/", commentController.index);
+router.get("/:id", commentController.show); */
 router.post("/", commentController.store);
-router.patch("/:id", commentController.update);
+/* router.patch("/:id", commentController.update); */
 router.delete("/:id", commentController.destroy);
 
 module.exports = router;
